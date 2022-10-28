9743e08389dbd9ec26c6ecdf5a565c576b02d7f2
Ramses Carvajal Pardo <116354886+Rakenov@users.noreply.github.com>
2022-10-28 10:13:35 -0300

    Primer bosquejo

---
+++ botilleria.py
@@ -0,0 +1,20 @@
+class Inventory
+  {
+  ##Inicio atributos##  
+  product_name:""
+  stock_quantity:0
+  critical_stock:0
+  expire_date:""
+  critical_date:""
+  product_price:""
+  bar_code:""
+  supplier:""
+    
+  
+  }
+  
+class Provider
+{
+
+
+}  
