5fe721fffa74296a4b8ca770ec2d987c9902f11c
Ramses Carvajal Pardo <116354886+Rakenov@users.noreply.github.com>
2022-10-31 21:19:34 -0300

    Inventory class

--- botilleria.py
+++ botilleria.py
@@ -1,20 +1,27 @@
 class Inventory
   {
-  ##Inicio atributos##  
+  ##Atributes##  
   product_name:""
   stock_quantity:0
   critical_stock:0
   expire_date:""
-  critical_date:""
+  expire_warning:""
   product_price:""
   bar_code:""
   supplier:""
+  description:""
+  n°_of_seals:""  #Numero de sellos negros  
     
-  
+  ##End Atributes##
+    
+  ##Methods##
+  update_name()
+  update_price()  
+  calculate_iva()
+  set_reestock_date()
+  count_expire_product()
+  show_data()  
+  ##End Methods##
   }
-  
-class Provider
-{
 
 
-}  
